2959b79a0d5018b72f5aa554e603e4de7ec50256
boyan.evtushok <boyan.evtushok@greent.games>
2021-03-08 14:22:16 +0700

    Cube followed for the cursor

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    public GameObject prefab;
+
+    GameObject selectedObject;
+    Vector3 currentCursorPoint;
+    Ray ray;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            var canSelectObject = Physics.Raycast(ray) && selectedObject == null;
+
+            selectedObject = canSelectObject ? Instantiate(prefab) : null;
+        }
+
+        SetObjPosToCursorPos(selectedObject);
+    }
+
+    void SetObjPosToCursorPos(GameObject gameObject)
+    {
+        if (gameObject == null) return;
+
+        currentCursorPoint = CalculateCurrentCursorPos();
+        gameObject.transform.position = currentCursorPoint;
+    }
+
+    Vector3 CalculateCurrentCursorPos()
+    {
+        currentCursorPoint = Input.mousePosition;
+        currentCursorPoint.z = -Camera.main.transform.position.z;
+        return Camera.main.ScreenToWorldPoint(currentCursorPoint);
+    }
+}
